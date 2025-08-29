82f7723b415c30c91e555cd6a55a353d25178b1a
Fluffy <cindyjuarez@Fluffys-MacBook-Pro.local>
2025-08-29 11:31:01 -0400

    vanguard parsing updates for lots

--- csv_parser/vanguard.py
+++ csv_parser/vanguard.py
@@ -3,21 +3,31 @@ import csv
 csv_file_path = 'cost_basis_vanguard_test_0.csv'
 symbol_column = 'Symbol/CUSIP'
 total_cost_column = 'Total cost'
-market_value_column_prefix = 'Market value as of'
+quantity_column = 'Quantity'
+
+class Lot:
+    def __init__(self, quantity, cost_basis):
+        self.quantity = quantity
+        self.cost_basis = cost_basis
 
 def parse_cost_basis_vanguard_csv():
+
+	lots_by_ticker: Dict[str, list[Lot]] = {}
 
 	with open(csv_file_path, mode='r', newline='', encoding='utf-8') as file:
 		next(file)
 		next(file)
 		reader = csv.DictReader(file)
 		headers = next(reader)
-		print(f"{headers=}")
-		market_value_columns = [col for col in headers if col.startswith(market_value_column_prefix)]
-		assert(len(market_value_columns) == 1)
-		market_value_column = market_value_columns[0]
 		for row in reader:
-		    symbol = row[symbol_column]
+		    ticker = row[symbol_column]
+		    quantity = row[quantity_column]
 		    cost_basis = row[total_cost_column]
-		    market_value = row[market_value_column]
-		    print(f"{symbol=}, {cost_basis=}, {market_value=}")
+		    if ticker not in lots_by_ticker:
+		    	lots_by_ticker[ticker] = []
+		    lots_by_ticker[ticker].append(Lot(quantity, cost_basis))
+		for lots in lots_by_ticker:
+			lots.sort(lambda x: x.cost_basis/x.quantity)
+			print(f"{ticker=}, {lots=}")
+
+	return lots_by_ticker
