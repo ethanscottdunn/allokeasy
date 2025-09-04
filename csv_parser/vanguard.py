92b82a02a289d8f8f4917384bf4cdacbe848ade2
Fluffy <cindyjuarez@Fluffys-MacBook-Pro.local>
2025-09-04 13:19:12 -0400

    add market value to ccsv parsing

--- csv_parser/vanguard.py
+++ csv_parser/vanguard.py
@@ -1,17 +1,19 @@
 import csv
 
 csv_file_path = 'csv_parser/cost_basis_vanguard_test_0.csv'
+market_value_column_prefix = 'Market value as of'
 symbol_column = 'Symbol/CUSIP'
 total_cost_column = 'Total cost'
 quantity_column = 'Quantity'
 
 class Lot:
-    def __init__(self, quantity, cost_basis):
+    def __init__(self, quantity, cost_basis, market_value):
         self.quantity = quantity
         self.cost_basis = cost_basis
+        self.market_value = market_value
 
     def __repr__(self):
-    	return f'{self.quantity=}, {self.cost_basis=}'
+    	return f'{self.quantity=}, {self.cost_basis=}, {self.market_value=}'
 
 def parse_cost_basis_vanguard_csv(csv_file_path=csv_file_path):
 
@@ -22,13 +24,20 @@ def parse_cost_basis_vanguard_csv(csv_file_path=csv_file_path):
 		next(file)
 		reader = csv.DictReader(file)
 		headers = next(reader)
+		market_value_column = ''
+		for header in headers:
+			if header.startswith(market_value_column_prefix):
+				market_value_column = header
+		if not market_value_column:
+			raise Exception('could not find a column that starts with prefix {market_value_column_prefix}')
 		for row in reader:
 		    ticker = row[symbol_column]
 		    quantity = float(row[quantity_column])
 		    cost_basis = float(row[total_cost_column])
+		    market_value = float(row[market_value_column])
 		    if ticker not in lots_by_ticker:
 		    	lots_by_ticker[ticker] = []
-		    lots_by_ticker[ticker].append(Lot(quantity, cost_basis))
+		    lots_by_ticker[ticker].append(Lot(quantity, cost_basis, market_value))
 		for lots in lots_by_ticker.values():
 			lots.sort(key=lambda x: x.cost_basis/x.quantity)
 
@@ -37,4 +46,5 @@ def parse_cost_basis_vanguard_csv(csv_file_path=csv_file_path):
 def parse_cost_basis_vanguard_csv_by_ticker(csv_file_path):
 
 	lots_by_ticker = parse_cost_basis_vanguard_csv(csv_file_path)
-	return {ticker: sum(lot.quantity for lot in lots) for ticker, lots in lots_by_ticker.items()}
+
+	return {'ticker': ticker, 'quantity': (sum(lot.quantity for lot in lots)), 'market_value': (sum(lot.market_value for lot in lots)) for ticker, lots in lots_by_ticker.items()}
