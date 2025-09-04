b86dbcd1870c75dee0a3ca1222a320320e4aca35
Emma Jablonski <e.jablonski@gmail.com>
2025-09-04 15:15:00 -0400

    added a form to process inputs for the portfolio optimization function

--- sallokeasy/allokeasy/urls.py
+++ sallokeasy/allokeasy/urls.py
@@ -5,4 +5,5 @@ from . import views
 urlpatterns = [
     path("", views.index, name="index"),
     path("upload-csv/", views.upload_csv, name="upload_csv"),
+    path("produce-optmized-portfolio/", views.produce_optimized_portfolio, name="produce_optimized_portfolio"),
 ]
--- sallokeasy/allokeasy/views.py
+++ sallokeasy/allokeasy/views.py
@@ -41,6 +41,52 @@ def upload_csv(request):
         #print((type(allocations)))
 
         #return JsonResponse({"allocations": allocations})
+        # TODO: put the file back in the import area...
         return JsonResponse({"allocations": alloc_dict_list})
 
     return JsonResponse({"error": "No file uploaded"}, status=400)
+
+@csrf_exempt
+def produce_optimized_portfolio(request):
+    if request.method == "POST" and request.FILES.get("file"):
+        csv_file = request.FILES["file"]
+        # Read file in memory
+        decoded_file = csv_file.read().decode("utf-8")
+        io_string = io.StringIO(decoded_file)
+        ## TODO: push this data into the function that computes new allocations and stats
+        alloc_dict_list = parse_cost_basis_vanguard_csv_by_ticker(io_string)
+
+    # Handle other input values
+    try:
+        income = float(request.POST.get("income"))
+        risk_free_rate = float(request.POST.get("risk_free_rate"))
+    except (TypeError, ValueError):
+        return JsonResponse({"error": "Invalid inputs for income and/or risk free rate."}, status=400)
+
+    # Handle dropdown
+    filing_status = request.POST.get("filing_status")
+    if filing_status not in ["single", "married_joint", "married_separate"]:
+        return JsonResponse({"error": "Invalid filing status."}, status=400)
+
+    # start_date & end_date
+    try:
+        start_date = request.POST.get("start_date")
+        end_date = request.POST.get("end_date")
+    except:
+        return JsonResponse({"error": "Invalid inputs for start and/or end date"})
+
+    ## TODO: run the optimization.
+    ## For now, just print the values so I know they came in right.
+    #print(alloc_dict_list)
+    print("income: %s" % income)
+    print("filing status: %s" % filing_status)
+    print("risk_free_rate: %s" % risk_free_rate)
+    print("start_date: %s" % start_date)
+    print("end_date: %s" % end_date)
+
+    #if request.method == "POST" and request.FILES.get("file"):
+        #return JsonResponse({"allocations": alloc_dict_list})
+    return JsonResponse({"error": "information received"}, status=400)
+
+
+
