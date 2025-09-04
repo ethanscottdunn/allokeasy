df750e458f6474e8c6a1878965360890982d7d90
Emma Jablonski <e.jablonski@gmail.com>
2025-09-04 10:25:54 -0400

    working vanguard parser in django server using utils file inside django project

--- sallokeasy/allokeasy/views.py
+++ sallokeasy/allokeasy/views.py
@@ -8,7 +8,8 @@ from django.views.decorators.csrf import csrf_exempt
 import csv
 import io
 from django.http import JsonResponse
-
+from utils.vanguard import parse_cost_basis_vanguard_csv_by_ticker
+import json
 
 def index(request):
     #return HttpResponse("Hello, world. You're at the allokeasy index.")
@@ -19,19 +20,24 @@ def upload_csv(request):
     if request.method == "POST" and request.FILES.get("file"):
         csv_file = request.FILES["file"]
         print("got into the function")
+       
         # Read file in memory
         decoded_file = csv_file.read().decode("utf-8")
         io_string = io.StringIO(decoded_file)
-        reader = csv.DictReader(io_string)
+        #reader = csv.DictReader(io_string)
+
+        alloc_dict = parse_cost_basis_vanguard_csv_by_ticker(io_string)
 
         allocations = []
-        for row in reader:
-            # Expect columns: name, percentage
+        for k, v in alloc_dict.items():
+            # Expect columns: ticker, value
             allocations.append({
-                "name": row.get("Description", ""),
-                "percentage": row.get("Percent gain loss", "")
+                "ticker": k,
+                "value": v
             })
         print(allocations)
+        print((type(allocations)))
+
         return JsonResponse({"allocations": allocations})
 
     return JsonResponse({"error": "No file uploaded"}, status=400)
---
+++ sallokeasy/utils/vanguard.py
@@ -0,0 +1,42 @@
+import csv
+
+csv_file_path = 'csv_parser/cost_basis_vanguard_test_0.csv'
+symbol_column = 'Symbol/CUSIP'
+total_cost_column = 'Total cost'
+quantity_column = 'Quantity'
+
+class Lot:
+    def __init__(self, quantity, cost_basis):
+        self.quantity = quantity
+        self.cost_basis = cost_basis
+
+    def __repr__(self):
+    	return f'{self.quantity=}, {self.cost_basis=}'
+
+def parse_cost_basis_vanguard_csv(csv_file_path):
+
+	lots_by_ticker: Dict[str, list[Lot]] = {}
+
+	#with open(csv_file_path, mode='r', newline='', encoding='utf-8') as file:
+	# it turns out the file is already opened
+	file = csv_file_path
+	next(file)
+	next(file)
+	reader = csv.DictReader(file)
+	headers = next(reader)
+	for row in reader:
+		ticker = row[symbol_column]
+		quantity = float(row[quantity_column])
+		cost_basis = float(row[total_cost_column])
+		if ticker not in lots_by_ticker:
+			lots_by_ticker[ticker] = []
+		lots_by_ticker[ticker].append(Lot(quantity, cost_basis))
+	for lots in lots_by_ticker.values():
+		lots.sort(key=lambda x: x.cost_basis/x.quantity)
+
+	return lots_by_ticker
+
+def parse_cost_basis_vanguard_csv_by_ticker(csv_file_path):
+
+	lots_by_ticker = parse_cost_basis_vanguard_csv(csv_file_path)
+	return {ticker: sum(lot.quantity for lot in lots) for ticker, lots in lots_by_ticker.items()}
