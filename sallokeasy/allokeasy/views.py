474dbc512045dfdb7d4d36db787335e5f64af004
Emma Jablonski <e.jablonski@gmail.com>
2025-09-05 09:42:54 -0400

    added optimize portfolio code to utils and updated the site to call the function

--- sallokeasy/allokeasy/views.py
+++ sallokeasy/allokeasy/views.py
@@ -9,6 +9,7 @@ import csv
 import io
 from django.http import JsonResponse
 from utils.vanguard import parse_cost_basis_vanguard_csv_by_ticker
+from utils.main_lot_level import compare_contrast_portfolios
 import json
 
 def index(request):
@@ -48,45 +49,55 @@ def upload_csv(request):
 
 @csrf_exempt
 def produce_optimized_portfolio(request):
-    if request.method == "POST" and request.FILES.get("file"):
-        csv_file = request.FILES["file"]
+    print(request.method)
+    if request.method == "POST" and request.FILES.get("csv_file"):
+        csv_file = request.FILES["csv_file"]
+        print("got into the function")
+       
         # Read file in memory
         decoded_file = csv_file.read().decode("utf-8")
         io_string = io.StringIO(decoded_file)
-        ## TODO: push this data into the function that computes new allocations and stats
-        alloc_dict_list = parse_cost_basis_vanguard_csv_by_ticker(io_string)
 
-    # Handle other input values
-    try:
-        income = float(request.POST.get("income"))
-        risk_free_rate = float(request.POST.get("risk_free_rate"))
-    except (TypeError, ValueError):
-        return JsonResponse({"error": "Invalid inputs for income and/or risk free rate."}, status=400)
+        # Handle other input values
+        try:
+            income = float(request.POST.get("income"))
+            risk_free_rate = float(request.POST.get("risk_free_rate"))
+        except (TypeError, ValueError):
+            return JsonResponse({"error": "Invalid inputs for income and/or risk free rate."}, status=400)
 
-    # Handle dropdown
-    filing_status = request.POST.get("filing_status")
-    if filing_status not in ["single", "married_joint", "married_separate"]:
-        return JsonResponse({"error": "Invalid filing status."}, status=400)
+        # Handle dropdown
+        filing_status = request.POST.get("filing_status")
+        if filing_status not in ["single", "married_joint", "married_separate"]:
+            return JsonResponse({"error": "Invalid filing status."}, status=400)
 
-    # start_date & end_date
-    try:
-        start_date = request.POST.get("start_date")
-        end_date = request.POST.get("end_date")
-    except:
-        return JsonResponse({"error": "Invalid inputs for start and/or end date"})
+        # start_date & end_date
+        try:
+            start_date = request.POST.get("start_date")
+            end_date = request.POST.get("end_date")
+        except:
+            return JsonResponse({"error": "Invalid inputs for start and/or end date"})
 
-    ## TODO: run the optimization.
-    ## For now, just print the values so I know they came in right.
-    #print(alloc_dict_list)
-    print("income: %s" % income)
-    print("filing status: %s" % filing_status)
-    print("risk_free_rate: %s" % risk_free_rate)
-    print("start_date: %s" % start_date)
-    print("end_date: %s" % end_date)
+        ## TODO: run the optimization.
+        ## For now, just print the values so I know they came in right.
+        #print(alloc_dict_list)
+        print("income: %s" % income)
+        print("filing status: %s" % filing_status)
+        print("risk_free_rate: %s" % risk_free_rate)
+        print("start_date: %s" % start_date)
+        print("end_date: %s" % end_date)
 
+        # io_string is csv_file -- not sure if this will fail
+        stuff = compare_contrast_portfolios(io_string, 
+                                            start_date, 
+                                            end_date, 
+                                            risk_free_rate, 
+                                            income, 
+                                            filing_status)
+        print(stuff)
+        return JsonResponse({"error": "information received"}, status=400)
     #if request.method == "POST" and request.FILES.get("file"):
         #return JsonResponse({"allocations": alloc_dict_list})
-    return JsonResponse({"error": "information received"}, status=400)
+    return JsonResponse({"error": "information not received"}, status=400)
 
 
 
