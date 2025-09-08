86a3c9fdbea4ebe64ceee547318a9d5730033b1b
Emma Jablonski <e.jablonski@gmail.com>
2025-09-08 13:31:59 -0400

    removing sallokeasy because replacing django project with basic cgi

--- sallokeasy/allokeasy/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AllokeasyConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'allokeasy'
--- sallokeasy/allokeasy/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-    path("", views.index, name="index"),
-    path("upload-csv/", views.upload_csv, name="upload_csv"),
-    path("produce-optmized-portfolio/", views.produce_optimized_portfolio, name="produce_optimized_portfolio"),
-]
